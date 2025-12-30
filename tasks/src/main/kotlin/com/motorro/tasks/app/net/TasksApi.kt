motorro/Android

Tasks API Refs: #4

tasks/src/main/kotlin/ru/merionet/tasks/app/net/TasksApi.kt
@@ -1,0 +1,83 @@
+package ru.merionet.tasks.app.net
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.parameters
+import kotlinx.coroutines.withContext
+import ru.merionet.core.coroutines.DispatcherProvider
+import ru.merionet.tasks.core.net.platformUrl
+import ru.merionet.tasks.data.HttpResponse
+import ru.merionet.tasks.data.TaskUpdateRequest
+import ru.merionet.tasks.data.TaskUpdates
+import ru.merionet.tasks.data.Version
+import ru.merionet.tasks.data.VersionResponse
+import ru.merionet.tasks.di.AppHttp
+import javax.inject.Inject
+
+/**
+ * Tasks API
+ */
+interface TasksApi {
+    /**
+     * Get current task-list version
+     */
+    suspend fun getVersion(): HttpResponse<VersionResponse>
+
+    /**
+     * Get task updates
+     */
+    suspend fun getUpdates(currentVersion: Version? = null): HttpResponse<TaskUpdates>
+
+    /**
+     * Puts task updates to server
+     */
+    suspend fun postUpdates(request: TaskUpdateRequest): HttpResponse<VersionResponse>
+
+    /**
+     * Ktor API implementation
+     */
+    class Impl @Inject constructor(
+        @AppHttp private val httpClient: HttpClient,
+        private val dispatchers: DispatcherProvider
+    ) : TasksApi {
+        override suspend fun getVersion(): HttpResponse<VersionResponse> {
+            val result = withContext(dispatchers.io) {
+                httpClient.get {
+                    platformUrl(listOf("tasks/version"))
+                }
+            }
+            return result.body()
+        }
+
+        override suspend fun getUpdates(currentVersion: Version?): HttpResponse<TaskUpdates> {
+            val result = withContext(dispatchers.io) {
+                httpClient.get {
+                    platformUrl(listOf("tasks/updates")) {
+                        parameters {
+                            currentVersion?.let {
+                                parameters["version"] = it.value
+                            }
+                        }
+                    }
+                }
+            }
+            return result.body()
+        }
+
+        override suspend fun postUpdates(request: TaskUpdateRequest): HttpResponse<VersionResponse> {
+            val result = withContext(dispatchers.io) {
+                httpClient.post {
+                    platformUrl(listOf("tasks/version"))
+                    contentType(ContentType.Application.Json)
+                    setBody(request)
+                }
+            }
+            return result.body()
+        }
+    }
+}

tasks/src/main/kotlin/ru/merionet/tasks/di/HttpModule.kt
@@ -1,0 +1,77 @@
+package ru.merionet.tasks.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import ru.merionet.tasks.app.net.TasksApi
+import ru.merionet.tasks.auth.SessionManager
+import ru.merionet.tasks.data.httpResponseModule
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class LoginHttp
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class AppHttp
+
+/**
+ * HTTP clients
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+class HttpModule {
+
+    @LoginHttp
+    @Provides
+    @Singleton
+    fun loginHttpClient(): HttpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json { serializersModule = httpResponseModule })
+        }
+    }
+
+    @AppHttp
+    @Provides
+    @Singleton
+    fun appHttpClient(sessionManager: SessionManager): HttpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json { serializersModule = httpResponseModule })
+        }
+        install(Auth) {
+            // Bearer token does not expire for this demo
+            // But you could automatically refresh it here
+            // See: https://ktor.io/docs/client-bearer-auth.html
+            bearer {
+                loadTokens {
+                    sessionManager.getTokens().run {
+                        BearerTokens(access, refresh)
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Network APIs
+ */
+@Module
+@InstallIn(ViewModelComponent::class)
+interface NetworkModule {
+    @Binds
+    fun tasksApi(impl: TasksApi.Impl): TasksApi
+}
+

merion-networks/android-course-demo

(cherry picked from commit 2e4db3543965b817081ea601f176a2959b3bbe55)

tasks/src/main/kotlin/com/motorro/tasks/app/net/TasksApi.kt
@@ -1,0 +1,83 @@
+package com.motorro.tasks.app.net
+
+import com.motorro.core.coroutines.DispatcherProvider
+import com.motorro.tasks.core.net.platformUrl
+import com.motorro.tasks.data.HttpResponse
+import com.motorro.tasks.data.TaskUpdateRequest
+import com.motorro.tasks.data.TaskUpdates
+import com.motorro.tasks.data.Version
+import com.motorro.tasks.data.VersionResponse
+import com.motorro.tasks.di.AppHttp
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.parameters
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+/**
+ * Tasks API
+ */
+interface TasksApi {
+    /**
+     * Get current task-list version
+     */
+    suspend fun getVersion(): HttpResponse<VersionResponse>
+
+    /**
+     * Get task updates
+     */
+    suspend fun getUpdates(currentVersion: Version? = null): HttpResponse<TaskUpdates>
+
+    /**
+     * Puts task updates to server
+     */
+    suspend fun postUpdates(request: TaskUpdateRequest): HttpResponse<VersionResponse>
+
+    /**
+     * Ktor API implementation
+     */
+    class Impl @Inject constructor(
+        @AppHttp private val httpClient: HttpClient,
+        private val dispatchers: DispatcherProvider
+    ) : TasksApi {
+        override suspend fun getVersion(): HttpResponse<VersionResponse> {
+            val result = withContext(dispatchers.io) {
+                httpClient.get {
+                    platformUrl(listOf("tasks/version"))
+                }
+            }
+            return result.body()
+        }
+
+        override suspend fun getUpdates(currentVersion: Version?): HttpResponse<TaskUpdates> {
+            val result = withContext(dispatchers.io) {
+                httpClient.get {
+                    platformUrl(listOf("tasks/updates")) {
+                        parameters {
+                            currentVersion?.let {
+                                parameters["version"] = it.value
+                            }
+                        }
+                    }
+                }
+            }
+            return result.body()
+        }
+
+        override suspend fun postUpdates(request: TaskUpdateRequest): HttpResponse<VersionResponse> {
+            val result = withContext(dispatchers.io) {
+                httpClient.post {
+                    platformUrl(listOf("tasks/version"))
+                    contentType(ContentType.Application.Json)
+                    setBody(request)
+                }
+            }
+            return result.body()
+        }
+    }
+}

tasks/src/main/kotlin/com/motorro/tasks/di/HttpModule.kt
@@ -1,0 +1,77 @@
+package com.motorro.tasks.di
+
+import com.motorro.tasks.app.net.TasksApi
+import com.motorro.tasks.auth.SessionManager
+import com.motorro.tasks.data.httpResponseModule
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class LoginHttp
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class AppHttp
+
+/**
+ * HTTP clients
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+class HttpModule {
+
+    @LoginHttp
+    @Provides
+    @Singleton
+    fun loginHttpClient(): HttpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json { serializersModule = httpResponseModule })
+        }
+    }
+
+    @AppHttp
+    @Provides
+    @Singleton
+    fun appHttpClient(sessionManager: SessionManager): HttpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json { serializersModule = httpResponseModule })
+        }
+        install(Auth) {
+            // Bearer token does not expire for this demo
+            // But you could automatically refresh it here
+            // See: https://ktor.io/docs/client-bearer-auth.html
+            bearer {
+                loadTokens {
+                    sessionManager.getTokens().run {
+                        BearerTokens(access, refresh)
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Network APIs
+ */
+@Module
+@InstallIn(ViewModelComponent::class)
+interface NetworkModule {
+    @Binds
+    fun tasksApi(impl: TasksApi.Impl): TasksApi
+}
+